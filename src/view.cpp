42a11fe6bbbc2a89b23d2d33d1a084c4749a01c3
Puno.Lee <punolee@outlook.com>
2018-12-09 12:03:52 -0600

    still a bug, user should not click jump too many times

--- src/view.cpp
+++ src/view.cpp
@@ -4,6 +4,8 @@
 
 #include "view.h"
 #include <iostream>
+#include <iomanip>
+
 using namespace ge211;
 
 View::View(Model const &model) : model_(model)
@@ -12,42 +14,47 @@ View::View(Model const &model) : model_(model)
 
 Dimensions View::screen_dimensions() const
 {
-    return {600, 200};
+    return {800, 200};
 }
 
 void View::draw(Sprite_set &sprites, bool onkeydown_) const
 {
 
     sprites.add_sprite(ground, Position(0, 188));
-    sprites.add_sprite(cloud, Position(30, 0));
-    sprites.add_sprite(cloud, Position(150, 0));
-    sprites.add_sprite(cloud, Position(300, 0));
+
+    for (int i = 0; i < 6; i++)
+    {
+        sprites.add_sprite(cloud, Position(30 + 150 * i, 0));
+    }
 
     sprites.add_sprite(dino, {model_.dino().x, 196 - model_.dino().y * 2});
 
-    for (auto itr : model_.ground_obstacles())
+    if (!model_.ground_obstacles().empty())
     {
-
-        switch (itr.tree_size_)
+        for (auto itr : model_.ground_obstacles())
         {
-        case 1:
-            sprites.add_sprite(one_ob, {itr.pos.x, 196 - itr.pos.y * 2});
-            break;
-        case 2:
-            sprites.add_sprite(two_ob, {itr.pos.x, 196 - itr.pos.y * 2});
-            break;
-        case 3:
-            sprites.add_sprite(three_ob, {itr.pos.x, 196 - itr.pos.y * 2});
-            break;
-        default:
-            break;
+
+            switch (itr.tree_size_)
+            {
+            case 1:
+                sprites.add_sprite(one_ob, {itr.pos.x, 146});
+                break;
+            case 2:
+                sprites.add_sprite(two_ob, {itr.pos.x, 146});
+                break;
+            case 3:
+                sprites.add_sprite(three_ob, {itr.pos.x, 146});
+                break;
+            default:
+                break;
+            }
         }
     }
 
     if (model_.is_game_over)
     {
-        sprites.add_sprite(text_game_over, Position(200, 100));
-        sprites.add_sprite(startover, Position(400, 100));
+        sprites.add_sprite(text_game_over, Position(300, 100));
+        sprites.add_sprite(startover, Position(500, 100));
     }
     //    if (onkeydown_){
     //
